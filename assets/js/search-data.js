aa4fda856f76ad1f3d911bc7c8cf74cc4664fadf
shalailahaas <shalailahaas@gmail.com>
2025-05-20 02:01:37 +0000

    Deploying to gh-pages from @ MINDLabResearch/mindlabresearch.github.io@5830af78627365e6737ece2ff6ce1d53766883e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -126,7 +126,7 @@ ninja.data = [{
           title: 'Debora Gonzalez',
           description: "Clinical Research Coordinator",
           section: "Team",handler: () => {
-              window.location.href = "/team/gonzalez/";
+              window.location.href = "/team/gonzales/";
             },},{id: "team-dr-shalaila-haas",
           title: 'Dr. Shalaila Haas',
           description: "Assistant Professor at Mount Sinai",
